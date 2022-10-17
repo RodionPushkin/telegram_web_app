edff283ffe5d6f9848464293df81ae63b1a02b00
Rodion <50911976+RodionPushkin@users.noreply.github.com>
2022-10-17 14:58:27 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -43,3 +43,4 @@ usercard.appendChild(profName); //добавляем
 let userid = document.createElement('p'); //создаем еще параграф 
 userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
 usercard.appendChild(userid); //добавляем
+tg.MainButton.enable()
